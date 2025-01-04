76f493fb49d6c2ef701b443a5b2e3f19c2d6c6d3
kelsonalmeida <kelson.victr@gmail.com>
2025-01-04 16:39:52 -0300

    routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import FornecedorList from './pages/Fornecedor/FornecedorList'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Inicial from './pages/Inicial'
 import FornecedorForm from './pages/Fornecedor/FornecedorForm'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
       <BrowserRouter>
-        <FornecedorList />
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Inicial />} />
+          <Route path="/listar-fornecedores" element={<FornecedorList />} />
+          <Route path="/add-fornecedores" element={<FornecedorForm />} />
+          <Route path="/edit-fornecedores/:id" element={<FornecedorForm />} />
+        </Routes>
       </BrowserRouter>
     
   )
