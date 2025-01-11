8ccd79eac94f7300d00bf9924f4cec01c90ca9a6
kelsonalmeida <kelson.victr@gmail.com>
2025-01-11 10:48:42 -0300

    useEffect

--- src/pages/Produto/ProdutoForm.jsx
+++ src/pages/Produto/ProdutoForm.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+import axios from '../../api'
 
 const ProdutoForm = () => {
 
@@ -18,6 +19,31 @@ const ProdutoForm = () => {
     const [tooltipAberto, setTooltipAberto] = useState(false)
     const navigate = useNavigate()
     const { id } = useParams()
+
+    useEffect(() => {
+        axios.get('/fornecedores')
+        .then(response => setFornecedores(response.data))
+        .catch(error => console.error("Erro ao buscar fornecedor", error))
+
+        //Se houver um ID, significa que vai editar, preciso buscar o produto a ser editado
+        if (id) {
+            axios.get(`/produtos/${id}`)
+            .then(response => setProduto({
+                ...response.data,
+                fornecedorId: response.data.fornecedor ? response.data.fornecedor.id : ''
+            }))
+            .catch(error => console.error("Ocorreu um erro: ", error))
+        } else {
+            setProduto({
+                nome: '',
+                preco: '',
+                descricao: '',
+                quantidadeEstoque: '',
+                fornecedorId: ''
+            })
+        }
+
+    }, [id])
 
   return (
     <div>ProdutoForm</div>
